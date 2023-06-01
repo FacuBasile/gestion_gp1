65790b13f1a3845b99f402ce99dfc41fd13ed2f6
AbreguJuan <abregu058@gmail.com>
2023-06-01 11:32:27 -0300

    Funcion ComprobarConeccion
    En el main cree una funcion comprobar coneccion para que probemos todos si nos podemos conectar a la base de datos.

--- src/gestion_gp1/Gestion_gp1.java
+++ src/gestion_gp1/Gestion_gp1.java
@@ -5,6 +5,11 @@
  */
 package gestion_gp1;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author H
@@ -15,7 +20,23 @@ public class Gestion_gp1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        comprobarConeccion();
+    }
+    
+    
+    //CONECCION
+    public static void comprobarConeccion () {
+        try {
+            Class.forName("org.mariadb.jdbc.Drver");
+            Connection conexion = DriverManager.getConnection("jdbc:mariadb://localhost/gestion_gp1","root","");
+            
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(null,"Error al cargar los Drivers");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"Error al conectarse a la Base de Datos");
+        }
+        //Si no salta ningun cartel "ANDA", sino "ALGO FALLA"
     }
     
 }
